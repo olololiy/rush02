6c8d1262982c7a01b9414539cd86f88681343212
DinislamDamirul <50663621+DinislamDamirul@users.noreply.github.com>
2020-08-09 20:35:01 +0300

    Add files via upload

---
+++ rush02/includes/numbers.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   numbers.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sevita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/09 19:17:36 by sevita            #+#    #+#             */
+/*   Updated: 2020/08/09 20:34:16 by sevita           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NUMBERS_H
+# define NUMBERS_H
+
+int		_1 = "one";
+int		_2 = "two";
+int		_3 = "three";
+int		_4 = "four";
+int		_5 = "five";
+int		_6 = "six";
+int		_7 = "seven";
+int		_8 = "eight";
+int		_9 = "nine";
+int		_10 = "ten";
+int		_11 = "eleven";
+int		_12 = "twelve";
+int		_13 = "thirteen";
+int		_14 = "fourteen";
+int		_15 = "fifteen";
+int		_16 = "sixteen";
+int		_17 = "seventeen";
+int		_18 = "eighteen";
+int		_19 = "nineteen";
+int		_20 = "twenty";
+int		_30 = "thirty";
+int		_40 = "fourty";
+int		_50 = "fifty";
+int		_60 = "sixty";
+int		_70 = "seventy";
+int		_80 = "eighty";
+int		_90 = "ninety";
+int		hundred = "hundred";
+int		thousand = "thousand";
+int		million = "million";
+int		billion = "billion";
+int		trillion = "trillion";
+int		quadrillion = "quadrillion";
+int		quintillion = "quintillion";
+int		sextillion = "sextillion";
+int		septillion = "septillion";
+int		octillion = "octillion";
+int		nonillion = "nonillion";
+int		decillion = "decillion";
+int		undecillion = "undecillion";
+#endif
