8af901bff94fbefed1a4738b39367e0f9ca5dae8
What-are-this <68893477+What-are-this@users.noreply.github.com>
2020-08-09 19:15:38 +0500

    Add files via upload

---
+++ rush02/sources/trim.c
@@ -0,0 +1,53 @@
+#include "rush02.h"
+
+char		*trim_left(char *str)
+{
+	while (*str && is_white_space(*str))
+		++str;
+	return (str);
+}
+
+void		trim_right(char *str)
+{
+	if (!*str)
+		return ;
+	while (*str)
+		{
+			++str;
+		}
+	--str;
+	while (is_white_space(*str))
+		--str;
+	*(++str) = 0;
+}
+
+char 		trim_side(char *str)
+{
+	char *ret;
+	char *start;
+	char *end;
+
+	if (!str)
+		return (0);
+	start = str;
+	while (*start && is_white_space(*start))
+		++start;
+	end = start;
+	while (*end)
+		++end;
+	--end;
+	while (is_white_space(*end))
+		--end;
+	ret = (char*)malloc(end - start + 2);
+	ft_strcpy(ret, start, end);
+	return (ret);
+}
+
+char		*trim_allocate_str(char *str)
+{
+	char *ret;
+
+	ret = trim_side(str);
+	free(str);
+	return (ret);
+}
